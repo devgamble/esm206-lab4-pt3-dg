7ba818d45694bd1380fbc0badb5cae9435514d68
devgamble <dgamble@ucsb.edu>
2019-10-21 13:56:54 -0700

    added a graph

--- lab4_part3.R
+++ lab4_part3.R
@@ -13,7 +13,17 @@ db0 <- read_csv(here("data", "disease_burden.csv")) %>%
   
 View(db0)
 
+#Create subset for comparison of infant death rates, then graph
 
+db_sub <- db0 %>% 
+  filter(country_name %in% c("United States", "Japan", "Afghanistan","Somalia")) %>% 
+  filter(age_group == "0-6 days", sex == "Both") #can use & instead of ,
+#Keep rows where country matches any of these
+# Check with unique(db_sub$country_name)
+
+#Create a basic line graph
+ggplot(data = db_sub, aes(x = year, y = deaths_per_100k))+
+  geom_line(aes(color = country_name))
 
 
 
